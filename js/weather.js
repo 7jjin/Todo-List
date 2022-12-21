886f56015906806ad789ce8c5602445713705586
7jjin <wlsgud6305@naver.com>
2022-12-21 19:43:52 +0900

    live_weather

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+
+const API_KEY = "01e682d294fd955bb78be36efcc69164";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      });
+  }
+  function onGeoError() {
+    alert("Can't find you. No weather for you.");
+  }
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
