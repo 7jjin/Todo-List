28d7215aec3ae55bfba6649612bb930fae6b5783
7jjin <wlsgud6305@naver.com>
2023-01-30 23:24:17 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -17,6 +17,6 @@ function onGeoOk(position) {
       });
   }
   function onGeoError() {
-    alert("Can't find you. No weather for you.");
+    alert("Can't find you. No weather for you.")
   }
   navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
